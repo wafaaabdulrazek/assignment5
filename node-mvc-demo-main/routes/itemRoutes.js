cf8d43c3ac02545a237152b9482f2ac044ff2ff6
wafaaabdulrazek <149031269+wafaaabdulrazek@users.noreply.github.com>
2025-03-30 23:37:22 -0700

    Add files via upload

---
+++ node-mvc-demo-main/routes/itemRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const itemController = require('../controllers/itemController');
+
+router.get('/', itemController.getItems);
+router.post('/add', itemController.addItem);
+// delete route
+router.post('/delete/:id', itemController.deleteItem);
+
+module.exports = router; 
